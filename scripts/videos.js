b4a1e4d9f71cbda6a79cac5b2d063eb047e9acc6
Robert <peas.sk@gmail.com>
2017-07-11 23:54:49 +0200

    Videos functionality

---
+++ scripts/videos.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    var $projects = $('.c-projects');
+    var $modal = $('#modal-video');
+    var $previews = $projects.find('.preview');
+
+    $previews.on('click', function(e){
+        $this = $(this);
+        $video = $this.find('.video-thumbnail');
+        imageName = $video.data('modal-img');
+        videoName = $video.data('modal-video');
+        videoElement = '<video class="test" poster="videos/' + imageName + '.jpg" controls>'
+                            + '<source src="videos/' + videoName + '.mp4" type="video/mp4">'
+                        + '</video>';
+        $modal.find('.modal-body').html(videoElement);
+        $modal.modal();
+        plyr.setup($modal.find('video')[0], {autoplay: true});
+    });
+
+    $previews.on('mousedown', function() {
+        $(this).addClass('clicked');
+    });
+
+    $previews.on('mouseup', function() {
+        $(this).removeClass('clicked');
+    });
+
+    $previews.on('mouseleave', function() {
+        $(this).removeClass('clicked');
+    });
+
+    $modal.on('hidden.bs.modal', function (e) {
+        var player = plyr.setup($modal.find('video')[0]);
+        player[0].stop();
+    })
+
+    //http://azmind.com/bootstrap-tutorial-modal-video/
+    //https://github.com/sampotts/plyr
+});
